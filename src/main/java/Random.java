80fc7a1b68ebfb689cc2636d563bb8f184531cc4
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-10-26 19:24:35 -0400

    made changes

---
+++ src/main/java/Random.java
@@ -0,0 +1,16 @@
+/**
+ * Created by iyasuwatts on 10/17/17.
+ */
+public class Random {
+
+    public static void main(String[] args){
+
+        //declaring fields using to be used with random number
+        //int num1 = 0;
+        int num2 = 50;
+        int ans = 27;
+    //I am declaring the math random method to help produce random numbers
+        int randomNumber = (int) Math.random() * num2;
+
+    }
+}
