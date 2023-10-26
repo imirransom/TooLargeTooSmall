d41dd58073846af0b7e7e92e44bfde8419ade7ab
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-10-26 19:52:52 -0400

    added changes

--- src/main/java/Random.java
+++ src/main/java/Random.java
@@ -9,20 +9,23 @@ public class Random {
     Scanner scanner = new Scanner(System.in);
         //Added  an object to use the scanner method
         System.out.print("Enter you number ");
-        int userNumber = scanner.nextInt();
+        int guess = scanner.nextInt();
         //This will be the max the random number will go up to
         int max = 50;
-    //I am declaring the math random method to help produce random numbers
+    //I am declaring the math random method to help produce random numbers. I am saving as the answer variable
         int ans = (int) Math.random() * max;
 
-        //I am starting a conditional
+        /*I am starting a conditional to check whether the user guesses the answer correctly and
+          If they are incorrect, it will keep them in an infinite loop until they guess correctly*/
         while (true){
-            if (userNumber < ans) {
-                System.out.println("Too Small");
+            if (guess < ans) {
+                System.out.println("Too small");
                 continue;
+            } else if (guess == ans) {
+                System.out.println("That is the correct answer!");
+                break;
             } else {
-                System.out.println("Too Large");
-                continue;
+                System.out.println("Too large");
             }
         }
 
