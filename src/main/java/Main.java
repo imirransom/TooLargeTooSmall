751a0d27d822658f4497d2a9b0b01c5c286e5026
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-10-26 14:03:17 -0400

    added fields to the method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,12 @@
 public class Main {
 
     public static void main(String[] args){
-        
+        //I mad ea
+        double randomNumber =  Math.random();
+
+        int num1 = 0;
+        int num2 = 50;
+        int correct = 27;
+
     }
 }
