3fe920cfa9c4b7b2696ba08d26f1c8394b62d479
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-10-26 19:40:35 -0400

    changes made

--- src/main/java/Random.java
+++ src/main/java/Random.java
@@ -1,16 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Random {
 
     public static void main(String[] args){
-
+    Scanner scanner = new Scanner(System.in);
         //declaring fields using to be used with random number
         //int num1 = 0;
+        System.out.print("Enter you number ");
+        int userNumber = scanner.nextLine();
         int num2 = 50;
-        int ans = 27;
     //I am declaring the math random method to help produce random numbers
-        int randomNumber = (int) Math.random() * num2;
-
+        int ans = (int) Math.random() * num2;
+        if (1 < ans) {
+            System.out.println();
+        }
     }
 }
